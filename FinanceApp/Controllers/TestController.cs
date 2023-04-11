2819e5520c2f49f1eab10e3d0eb235432454ebb1
Kenneth Okalang <okalang.ok@gmail.com>
2023-04-12 00:20:25 +0200

    -Testing connectivity -uppdate appsettings.production.

---
+++ FinanceApp/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FinanceApp.Data; // Replace YourNamespace with your project's namespace
+
+namespace FinanceApp.Controllers // Replace YourNamespace with your project's namespace
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TestController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("test-connection")]
+        public IActionResult TestConnection()
+        {
+            try
+            {
+                _context.Database.ExecuteSqlRaw("SELECT 1");
+                return Ok("Database connection successful.");
+            }
+            catch (System.Exception ex)
+            {
+                return StatusCode(500, $"Database connection failed. Error: {ex.Message}");
+            }
+        }
+    }
+}
