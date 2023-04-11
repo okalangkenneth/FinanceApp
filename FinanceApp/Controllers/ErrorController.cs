cb83aee044579ebc450fe307ea57eef513324dd8
Kenneth Okalang <okalang.ok@gmail.com>
2023-04-11 20:51:59 +0200

    - Add custom error page and ErrorController

---
+++ FinanceApp/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Sorry, the resource you requested could not be found.";
+                    break;
+                case 500:
+                    ViewBag.ErrorMessage = "Sorry, something went wrong on the server.";
+                    break;
+                // Add more cases for other status codes if needed
+                default:
+                    ViewBag.ErrorMessage = "An unexpected error occurred.";
+                    break;
+            }
+            return View("Error");
+        }
+
+    }
+}
--- FinanceApp/Startup.cs
+++ FinanceApp/Startup.cs
@@ -118,7 +118,7 @@ namespace FinanceApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -129,6 +129,9 @@ namespace FinanceApp
 
             app.UseAuthentication();
             app.UseAuthorization();
+
+            app.UseStatusCodePagesWithReExecute("/Error/{0}");
+
 
             app.UseEndpoints(endpoints =>
             {
